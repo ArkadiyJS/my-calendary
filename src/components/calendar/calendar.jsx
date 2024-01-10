bb81f6358be38ac5b3be8747be080be2963ad2c9
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-01-10 19:35:09 +0500

    start

---
+++ src/components/calendar/calendar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import s from './calendar.module.css'
+
+function Calendar(props) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <div className={s.calendar}>
+
+      <header>
+        <button>{`<`}</button>
+
+
+        <select></select>
+        <select></select>
+
+
+        <button>{`>`}</button>
+      </header>
+
+      <table>
+        <thead>
+          <tr>
+
+          </tr>
+        </thead>
+
+        <tbody>
+
+
+        </tbody>
+      </table>
+
+
+    </div>
+  );
+}
+
+export default Calendar;

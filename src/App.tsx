a0f14d7c9267312e98fc812cd7cb64f44b71e3f5
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-01-12 20:36:57 +0500

    clear out filles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import 'react-calendar/dist/Calendar.css';
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import DayEvent from './components/DayEvent.tsx';
-import Test from './components/test.jsx';
+
 
 
 type ValuePiece = Date | null;
